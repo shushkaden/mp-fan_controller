9e87b03a8ffe6d79e6e5222e703ee7814676fdcf
Denis Shushkevich <d_shushkevich@wargaming.net>
2020-06-13 00:35:19 +0300

    add button

---
+++ src/button.py
@@ -0,0 +1,19 @@
+from machine import Pin
+
+
+class Button:
+    pin = None
+    value = 1
+    action = None
+
+    def __init__(self, pin, action):
+        self.action = action
+        self.pin = Pin(pin, Pin.IN)
+        self.pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.callback)
+
+    def callback(self, _):
+        new_value = self.pin.value()
+        if self.value == 1 and new_value == 0:
+            self.action()
+
+        self.value = new_value
