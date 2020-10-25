bb1350763a3811232456db4dba7e85763678ac04
Denis Shushkevich <d_shushkevich@wargaming.net>
2020-10-25 20:42:39 +0300

    set datetime

--- src/timemodule.py
+++ src/timemodule.py
@@ -6,3 +6,12 @@ time_module = DS3231(I2C(sda=Pin(21), scl=Pin(22)))
 
 def now():
     return time_module.DateTime()
+
+
+def set_datetime(year, month, day, hour, minute, second):
+    time_module.Year(year)
+    time_module.Month(month)
+    time_module.Day(day)
+    time_module.Hour(hour)
+    time_module.Minute(minute)
+    time_module.Second(second)
